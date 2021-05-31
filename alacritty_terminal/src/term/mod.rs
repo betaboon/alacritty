c5b7304e93d94c0dee4b07f2741b07b6a9cef5e1
Ayose <ayosec@gmail.com>
2021-05-31 20:53:23 +0100

    Define MAX_GRAPHIC_DIMENSIONS as a 2-elements array.

--- alacritty_terminal/src/term/mod.rs
+++ alacritty_terminal/src/term/mod.rs
@@ -1786,7 +1786,7 @@ impl<T: EventListener> Handler for Term<T> {
         let (ps, pv) = if pa == 1 || pa == 4 {
             match pi {
                 1 => (0, &[sixel::MAX_COLOR_REGISTERS][..]),
-                2 => (0, &[MAX_GRAPHIC_DIMENSIONS.0, MAX_GRAPHIC_DIMENSIONS.1][..]),
+                2 => (0, &MAX_GRAPHIC_DIMENSIONS[..]),
                 _ => (1, &[][..]), // Report error in Pi
             }
         } else {
@@ -1815,7 +1815,7 @@ impl<T: EventListener> Handler for Term<T> {
             }
         }
 
-        if graphic.width > MAX_GRAPHIC_DIMENSIONS.0 || graphic.height > MAX_GRAPHIC_DIMENSIONS.1 {
+        if graphic.width > MAX_GRAPHIC_DIMENSIONS[0] || graphic.height > MAX_GRAPHIC_DIMENSIONS[1] {
             return;
         }
 
